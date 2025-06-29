94d81ea2a03cce851b7c4b3b4f030d96f0a655a0
NikhilBansalv <nikhilbansal536@gmail.com>
2025-06-29 14:27:49 +0530

    updating backend

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,13 @@
 import os
 from pymongo import MongoClient
+import certifi
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/matchdb")
-client = MongoClient(MONGO_URI)
-db = client["matchmaking"]
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
+db = client["matchdb"]
 users_collection = db["users"]
 comparisons_collection = db["comparisons"]
