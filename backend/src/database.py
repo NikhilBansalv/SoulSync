ca37134f20c2de6e23c9f9326c2fdd24baf617a4
NikhilBansalv <nikhilbansal536@gmail.com>
2025-06-29 14:18:34 +0530

    updated database url

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,8 @@
-# backend/src/database.py
+import os
 from pymongo import MongoClient
 
-# Connect to local MongoDB
-client = MongoClient("mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/matchdb")
+client = MongoClient(MONGO_URI)
 db = client["matchmaking"]
 users_collection = db["users"]
 comparisons_collection = db["comparisons"]
