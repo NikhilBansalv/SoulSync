81fb8d54782c96c3a6b45816157c63beb6afa9ff
NikhilBansalv <nikhilbansal536@gmail.com>
2025-06-29 14:51:02 +0530

    Revert "updating backend"
    This reverts commit 94d81ea2a03cce851b7c4b3b4f030d96f0a655a0.

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,13 +1,8 @@
 import os
 from pymongo import MongoClient
-import certifi
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/matchdb")
-client = MongoClient(
-    MONGO_URI,
-    tls=True,
-    tlsCAFile=certifi.where()
-)
-db = client["matchdb"]
+client = MongoClient(MONGO_URI)
+db = client["matchmaking"]
 users_collection = db["users"]
 comparisons_collection = db["comparisons"]
